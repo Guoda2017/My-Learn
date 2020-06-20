9bc1f561f7da5d106ea48e0b18b08f665bc36e1d
guofengbo <guofengbo@youxin.com>
2020-06-20 23:53:00 +0800

    opt 冒泡排序

---
+++ data-agency/src/main/java/com/guo/ListNode.java
@@ -0,0 +1,17 @@
+package com.guo;
+
+/**
+ * @Description:
+ * @author: guofengbo
+ * @date: 2020-06-16 23:36
+ **/
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
---
+++ data-agency/src/main/java/com/guo/Solution.java
@@ -0,0 +1,16 @@
+package com.guo;
+
+/**
+ * @Description:
+ * @author: guofengbo
+ * @date: 2020-06-16 23:35
+ **/
+public class Solution {
+
+
+    public ListNode reverseList(ListNode head) {
+
+        return null;
+
+    }
+}
