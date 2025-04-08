733e21cca3585dda74be7febfc92c1ccb777facb
fengbo <903136249@qq.com>
2025-04-08 14:17:02 +0800

    test jdbc

--- object-demo/src/main/java/com/guo/Test.java
+++ object-demo/src/main/java/com/guo/Test.java
@@ -2,13 +2,8 @@ package com.guo;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * @author : fengbo.guo
@@ -18,60 +13,13 @@ import java.util.List;
 @Slf4j
 public class Test {
 
-    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM");
-    private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    private static ThreadLocal<SimpleDateFormat> threadDateTime = new ThreadLocal<SimpleDateFormat>();
 
     public static void main(String[] args) {
 
-        boolean b = checkNowInTimePeriod(Collections.singletonList("17:40-18:50"));
-        System.out.println("===========>" + b);
-    }
+        BigDecimal amount1 = new BigDecimal("0.00033").multiply(new BigDecimal("709749")).setScale(2, RoundingMode.DOWN);
+        BigDecimal amount2 = new BigDecimal("0.00033").multiply(new BigDecimal("709945")).setScale(2, RoundingMode.DOWN);
 
-    private static SimpleDateFormat DateTimeInstance() {
-        SimpleDateFormat df = threadDateTime.get();
-        if (df == null) {
-            df = new SimpleDateFormat(DATETIME_FORMAT);
-            threadDateTime.set(df);
-        }
-        return df;
-    }
-
-    public static boolean checkNowInTimePeriod(List<String> timePeriodList) {
-
-        // 获取当前时间
-        LocalTime now = LocalTime.now();
-
-        for (String timePeriod : timePeriodList) {
-
-            try {
-                String[] split = timePeriod.split("-");
-                String startTimeStr = split[0].trim();
-                String endTimeStr = split[1].trim();
-                // 将字符串转换成LocalTime对象
-                LocalTime startTime = LocalTime.of(Integer.parseInt(startTimeStr.split(":")[0]), Integer.parseInt(startTimeStr.split(":")[1]));
-                LocalTime endTime = LocalTime.of(Integer.parseInt(endTimeStr.split(":")[0]), Integer.parseInt(endTimeStr.split(":")[1]));
-
-                if ((now.isAfter(startTime) && now.isBefore(endTime))) {
-                    return true;
-                }
-            } catch (Exception e) {
-                log.error("解析时间格式异常,请关注", e);
-                return false;
-            }
-        }
-        return false;
-    }
-
-    private static boolean between(String startTime, String endTime, Date date) throws ParseException {
-        return between(dateTime(startTime), dateTime(endTime), date);
-    }
-
-    private static boolean between(Date startTime, Date endTime, Date date) {
-        return date.after(startTime) && date.before(endTime);
-    }
-
-    private static Date dateTime(String datestr) throws ParseException {
-        return DateTimeInstance().parse(datestr);
+        System.out.println("amount1 : " + amount1 + "amount2 :" + amount2);
     }
 }
+
--- object-demo/src/main/java/com/guo/TestJdbc.java
+++ object-demo/src/main/java/com/guo/TestJdbc.java
@@ -1,120 +1,134 @@
-package com.guo;
-
-import lombok.extern.slf4j.Slf4j;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-/**
- * @author : fengbo.guo
- * @date : 2024-11-11 17:11
- * @Description :
- */
-@Slf4j
-public class TestJdbc {
-
-    public static void main(String[] args) {
-        // 数据库1的配置
-        String url1 = "jdbc:mysql://rm-3ns8l5ow758010z4x.mysql.rds.aliyuncs.com:3303/za_bank_stock_trade";
-        String user1 = "za_bank_stock_trade";
-        String password1 = "LR^XzsVgooC5Scdn";
-
-        List<String> userIdList = new ArrayList<>();
-
-        // 分页参数
-        int pageNum = 1; // 页码
-        int pageSize = 5000; // 每页大小
-
-        // 从数据库1分页拉取数据
-        boolean hasMoreData = true;
-        while (hasMoreData) {
-            hasMoreData = fetchDataFromDb1(url1, user1, password1, pageNum, pageSize, userIdList);
-            pageNum++; // 自增页码以获取下一页的数据
-            userIdList = userIdList.stream().distinct().collect(Collectors.toList());
-            System.out.println("需要处理的userIdList:" + userIdList);
-        }
-
-    }
-
-    private static boolean fetchDataFromDb1(String url1, String user1, String password1, int pageNum, int pageSize, List<String> userIdList) {
-
-        // 数据库2的配置
-        String url2 = "jdbc:mysql://rm-3ns8l5ow758010z4x.mysql.rds.aliyuncs.com:3303/za_bank_fund_account";
-        String user2 = "za_bank_ftc_uat";
-        String password2 = "QYhKw0r^6FXP7N7L";
-        Map<String, String> holdingMap = new HashMap();
-
-        String sql1 = "SELECT user_id,account_id FROM stk_holding where is_deleted ='N' LIMIT ?, ?";
-
-        try (Connection conn1 = DriverManager.getConnection(url1, user1, password1);
-             PreparedStatement pstmt1 = conn1.prepareStatement(sql1)) {
-
-            pstmt1.setInt(1, (pageNum - 1) * pageSize);
-            pstmt1.setInt(2, pageSize);
-            ResultSet rs = pstmt1.executeQuery();
-
-            while (rs.next()) {
-                String userId = rs.getString("user_id");
-                String accountId = rs.getString("account_id");
-                holdingMap.put(userId, accountId);
-            }
-
-            System.out.println("pageSize:" + pageSize + "pageNum:" + pageNum);
-
-            // 对于数据库1中的每条数据，检查数据库2中是否存在
-            Map<String, String> userMap = checkDataInDb2(url2, user2, password2, holdingMap);
-
-            List<String> notInPageList = holdingMap.keySet().stream().filter(key -> !userMap.get(key).equals(holdingMap.get(key))).collect(Collectors.toList());
-
-//            System.out.println("holdingMap:" + holdingMap);
-//            System.out.println("userMap:" + userMap);
-            System.out.println("notInPageList:" + notInPageList);
-
-//            if (CollectionUtils.isNotEmpty(notInPageList)) {
-//                System.out.println("Data: " + notInPageList + " does not exist in DB2.");
-//                String sql2 = "update stk_holding set is_deleted ='Y' where  user_id = ?";
-//                for (String userId : notInPageList) {
+//package com.guo;
 //
-//                    PreparedStatement pstmt2 = conn1.prepareStatement(sql2);
+//import lombok.SneakyThrows;
+//import lombok.extern.slf4j.Slf4j;
 //
-//                    pstmt2.setString(1, userId);
-//                    pstmt2.executeUpdate();
-//                    System.out.println("删除了数据" + userId);
+//import java.sql.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//import java.util.concurrent.Future;
+//import java.util.concurrent.TimeUnit;
+//import java.util.stream.Collectors;
+//
+///**
+// * @author : fengbo.guo
+// * @date : 2024-11-11 17:11
+// * @Description :
+// */
+//@Slf4j
+//public class TestJdbc {
+//
+//    public static void main(String[] args) {
+//        // 数据库1的配置
+//        String url1 = "jdbc:mysql://rm-3ns8l5ow758010z4x.mysql.rds.aliyuncs.com:3303/za_bank_stock_trade";
+//        String user1 = "za_bank_stock_trade";
+//        String password1 = "LR^XzsVgooC5Scdn";
+//
+//
+//        // 分页参数
+//        int pageNum = 1; // 页码
+//        int pageSize = 100; // 每页大小
+//
+//        ExecutorService executor = Executors.newFixedThreadPool(20);
+//
+//        // 从数据库1分页拉取数据
+//        boolean hasMoreData = true;
+//        List<Future<List<String>>> futures = new ArrayList<>();
+//        while (hasMoreData) {
+//            hasMoreData = fetchDataFromDb1(futures,url1, user1, password1, pageNum, pageSize);
+//            pageNum++; // 自增页码以获取下一页的数据
+//        }
+//        // 关闭线程池
+//        executor.shutdown();
+//        try {
+//            executor.awaitTermination(60, TimeUnit.SECONDS);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @SneakyThrows
+//    private static boolean fetchDataFromDb1(List<Future<List<String>>> futures, String url1, String user1, String password1, int pageNum, int pageSize) {
+//        // 数据库2的配置
+//        String url2 = "jdbc:mysql://rm-3ns8l5ow758010z4x.mysql.rds.aliyuncs.com:3303/za_bank_stock_trade";
+//        String user2 = "za_bank_stock_trade";
+//        String password2 = "LR^XzsVgooC5Scdn";
+//        List<String> list = new ArrayList<>();
+//        String sql1 = "SELECT account_id FROM stk_holding where is_deleted ='N' and hold_qty >0 group by account_id LIMIT ?, ?";
+//
+//        int total = 0;
+//
+//        try (Connection conn1 = DriverManager.getConnection(url1, user1, password1);
+//             PreparedStatement pstmt1 = conn1.prepareStatement(sql1)) {
+//
+//            pstmt1.setInt(1, (pageNum - 1) * pageSize);
+//            pstmt1.setInt(2, pageSize);
+//            ResultSet rs = pstmt1.executeQuery();
+//
+//            while (rs.next()) {
+//                String accountId = rs.getString("account_id");
+//                total = total + 1;
+//                // 提交任务到线程池
+//                futures.a
+//                Future<List<String>> submit = executor.submit(() -> checkDataInDb2(url2, user2, password2, accountId));
+//
+//                log.info("account_id" + accountId);
+//                log.info("total" + total);
+//                log.info("list" + list.addAll(submit.get()));
+//            }
+//
+//            System.out.println("pageSize:" + pageSize + "pageNum:" + pageNum);
+//
+//            // 数据不存在时的操作
+//            return !rs.isLast();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
+//
+//    private static List<String> checkDataInDb2(String url2, String user2, String password2, String accountId) {
+//        List<String> historyList = new ArrayList<>();
+//        List<String> errorList = new ArrayList<>();
+//        String sql2 = "SELECT stock_code from stk_holding_history where trade_date = '2024-11-26' and is_deleted = 'N' and account_id = ?";
+//        String sql3 = "SELECT stock_code from stk_holding where hold_qty > 0 and is_deleted = 'N' and account_id = ?";
+//
+//        try (Connection conn2 = DriverManager.getConnection(url2, user2, password2);
+//             PreparedStatement pstmt2 = conn2.prepareStatement(sql2)) {
+//
+//            pstmt2.setString(1, accountId);
+//            ResultSet rs = pstmt2.executeQuery();
+//
+//            while (rs.next()) {
+//                String stockCode = rs.getString("stock_code");
+//                historyList.add(stockCode);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        log.info("accountId:{} historyList:{}", accountId, historyList);
+//
+//        try (Connection conn3 = DriverManager.getConnection(url2, user2, password2);
+//             PreparedStatement pstmt3 = conn3.prepareStatement(sql3)) {
+//
+//            pstmt3.setString(1, accountId);
+//            ResultSet rs = pstmt3.executeQuery();
+//
+//            while (rs.next()) {
+//                String stockCode = rs.getString("stock_code");
+//                if (!historyList.contains(stockCode)) {
+//                    errorList.add(accountId + stockCode);
 //                }
 //            }
-
-            // 数据不存在时的操作
-            userIdList.addAll(notInPageList);
-            return !rs.isLast();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    private static Map<String, String> checkDataInDb2(String url2, String user2, String password2, Map<String, String> holdingMap) {
-        Map<String, String> userMap = new HashMap<>();
-        String sql2 = "SELECT bank_user_id,broker_account_id FROM user_invest_account WHERE account_type = 'stock' and bank_user_id " +
-                "IN (" + holdingMap.keySet().stream().map(v -> "?").collect(Collectors.joining(",")) + ")";
-        try (Connection conn2 = DriverManager.getConnection(url2, user2, password2);
-             PreparedStatement pstmt2 = conn2.prepareStatement(sql2)) {
-
-            int parameterIndex = 1;
-            for (String value : holdingMap.keySet()) {
-                pstmt2.setString(parameterIndex++, value);
-            }
-            ResultSet rs = pstmt2.executeQuery();
-
-            while (rs.next()) {
-                userMap.put(rs.getString("bank_user_id"), rs.getString("broker_account_id"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return userMap;
-    }
-}
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // 处理 errorList，例如打印或存储
+//        log.info("Error List for {}: {}", accountId, errorList);
+//        return errorList;
+//    }
+//}
